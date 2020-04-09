643b1e54d2e3851aebc8b70efff3dca561f7ad39
unknown <levantuan6898@gmail.com>
2020-04-09 11:17:03 +0700

    add file

---
+++ src/Interest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Interest {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interest_rate = 1.0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = sc.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = sc.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interest_rate = sc.nextDouble();
+        double total_interset = 0;
+        for(int i = 0; i < month; i++){
+            total_interset = money * (interest_rate/100)/12 * 3;
+        }
+        System.out.println("Total of interset: " + total_interset);
+    }
+}
